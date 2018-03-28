0234648ae0a26971127566bc87f1a5576f8b450a
Christopher McIntyre <me@cgmcintyre.com>
2018-03-28 14:46:42 +0100

    Duplicate query tests for query results

--- test/lib/db/kairos/kairos_query_test.exs
+++ test/lib/db/kairos/kairos_query_test.exs
@@ -16,4 +16,20 @@ defmodule KairosQueryTest do
       assert Poison.decode!(data, as: %KairosQuery{}) == expected
     end
   end
+
+  describe "Poison.encode! KairosQueryResult" do
+    test "Encoding entire KairosQueryResult returns json object" do
+      query_result = %KairosQueryResult{name: "test", group_by: [], tags: %{}, values: []}
+      expected = ~s({"values":[],"tags":{},"name":"test","group_by":[]})
+      assert Poison.encode!(query_result) == expected
+    end
+  end
+
+  describe "Poison.decode! KairosQueryResult" do
+    test "Decoding entire KairosQueryResult returns json object" do
+      data = ~s({"values":[],"tags":{},"name":"test","group_by":[]})
+      expected = %KairosQueryResult{name: "test", group_by: [], tags: %{}, values: []}
+      assert Poison.decode!(data, as: %KairosQueryResult{}) == expected
+    end
+  end
 end
