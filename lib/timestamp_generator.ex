67b9065cc2fc9524da685095c0edd25ce7ffa3b4
Christopher McIntyre <me@cgmcintyre.com>
2018-03-29 14:00:37 +0100

    Add TimestampGenerator

---
+++ lib/timestamp_generator.ex
@@ -0,0 +1,28 @@
+defmodule TimestampGenerator do
+  def new(start_time, interval) do
+    Agent.start_link(fn ->
+      %{start_time: start_time, interval: interval, timestamp: start_time}
+    end)
+  end
+
+  def get_timestamp(pid) do
+    Agent.get(pid, &Map.get(&1, :timestamp))
+  end
+
+  def get_interval(pid) do
+    Agent.get(pid, &Map.get(&1, :interval))
+  end
+
+  def get_start_time(pid) do
+    Agent.get(pid, &Map.get(&1, :start_time))
+  end
+
+  def next_timestamp(pid) do
+    Agent.get_and_update(pid, fn map ->
+      {:ok, i} = Map.fetch(map, :interval)
+      new_map = Map.update!(map, :timestamp, fn value -> value + i end)
+      {:ok, ts} = Map.fetch(new_map, :timestamp)
+      {ts, new_map}
+    end)
+  end
+end
---
+++ test/lib/timestamp_generato_test.exs
@@ -0,0 +1,36 @@
+defmodule TimestampGeneratorTest do
+  use ExUnit.Case
+
+  test "Getting start_time of new TimestampGenerator returns initialised start_time" do
+    start_time = 1000
+    {:ok, pid} = TimestampGenerator.new(start_time, 2000)
+    assert TimestampGenerator.get_start_time(pid) == start_time
+  end
+
+  test "Getting interval of new TimestampGenerator returns initialised interval" do
+    interval = 1000
+    {:ok, pid} = TimestampGenerator.new(2000, interval)
+    assert TimestampGenerator.get_interval(pid) == interval
+  end
+
+  test "Timestamp of new TimestampGenerator is initialised start_time" do
+    start_time = 1000
+    {:ok, pid} = TimestampGenerator.new(start_time, 2000)
+    assert TimestampGenerator.get_timestamp(pid) == start_time
+  end
+
+  test "TimestampGenerator.next increments timestamp by interval" do
+    start_time = 1000
+    interval = 2000
+    {:ok, pid} = TimestampGenerator.new(start_time, interval)
+    assert TimestampGenerator.next_timestamp(pid) == start_time + interval
+  end
+
+  test "Calling TimestampGenerator.next twice increments timestamp by interval * 2" do
+    start_time = 1000
+    interval = 2000
+    {:ok, pid} = TimestampGenerator.new(start_time, interval)
+    TimestampGenerator.next_timestamp(pid)
+    assert TimestampGenerator.next_timestamp(pid) == start_time + interval * 2
+  end
+end
