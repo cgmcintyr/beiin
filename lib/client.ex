5341bf2b9bf47f23a91c0967d5e74521eacddd1e
Christopher McIntyre <me@cgmcintyre.com>
2018-03-29 15:18:42 +0100

    Updated client to insert/read 10 times

--- lib/client.ex
+++ lib/client.ex
@@ -5,11 +5,23 @@ defmodule Client do
 
   def run(msg, opts \\ []) do
     Logger.info(fn -> "Running client" end)
+    {:ok, tsg_pid} = TimestampGenerator.new(1522331174000, 1000)
+
     {db, opts} = Keyword.pop(opts, :database, @default_database)
     db.init("localhost", 8080)
-    db_ins = Currying.curry_many(&db.insert/5, ['localhost', 8080])
-    db_ins.("localhost").(1235).(1235)
-    db_read = Currying.curry_many(&db.read/4, ['localhost', 8080])
-    db_read.("localhost").(1235)
+
+    read = Currying.curry_many(&db.insert/5, ['localhost', 8080])
+    insert = Currying.curry_many(&db.read/4, ['localhost', 8080])
+
+    loop("localhost", tsg_pid, read, insert, 5)
+  end
+
+  defp loop(_, _,  _, _, 0) do
+  end
+
+  defp loop(metric, tsg_pid, read_fun, ins_fun, n) do
+    read_fun.(metric).(TimestampGenerator.next_timestamp(tsg_pid)).(1000)
+    ins_fun.(metric).(TimestampGenerator.get_timestamp(tsg_pid))
+    loop(metric, tsg_pid, read_fun, ins_fun, n - 1)
   end
 end
