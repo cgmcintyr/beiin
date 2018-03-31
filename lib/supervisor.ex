0654f6dac03720459e6e5f37e67d2b3d04e9897a
Christopher McIntyre <me@cgmcintyre.com>
2018-03-31 12:18:39 +0100

    Add supervisor for RecordServer

---
+++ lib/supervisor.ex
@@ -0,0 +1,18 @@
+defmodule RecordServerSupervisor do
+  use Supervisor
+
+  @metrics ["metric_1", "metric_2"]
+  @tags %{host: "default"}
+
+  def start_link(opts) do
+    Supervisor.start_link(__MODULE__, :ok, opts)
+  end
+
+  def init(:ok) do
+    children = [
+      %{id: RecordServer, start: {RecordServer, :start_link, [@metrics, @tags, 1000, 1000, [name: RecordServer]]}},
+    ]
+
+    Supervisor.init(children, strategy: :one_for_one)
+  end
+end
