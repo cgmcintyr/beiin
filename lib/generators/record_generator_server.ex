4cdea7a8926c80fb43ef388afb425210948adf28
Christopher McIntyre <me@cgmcintyre.com>
2018-04-02 12:56:01 +0100

    Add next_read

--- lib/generators/record_generator_server.ex
+++ lib/generators/record_generator_server.ex
@@ -9,12 +9,15 @@ defmodule RecordServer do
 
   def start_link(metrics, tag_maps, opts \\ []) do
     {ins_tsg, opts} = Keyword.pop(opts, :ins_tsg, TimestampGenerator)
+    {read_tsg, opts} = Keyword.pop(opts, :read_tsg, TimestampGenerator)
 
     init_map = %{
       metrics: metrics,
       next_ins_records: [],
+      next_read_records: [],
       tag_maps: tag_maps,
-      tsg_ref: ins_tsg
+      ins_tsg: ins_tsg,
+      read_tsg: read_tsg
     }
 
     GenServer.start_link(__MODULE__, init_map, opts)
@@ -24,12 +27,16 @@ defmodule RecordServer do
     GenServer.call(server, {:next_insert})
   end
 
+  def next_read(server) do
+    GenServer.call(server, {:next_insert})
+  end
+
   ## Server Callbacks
   def init(map) do
     {:ok, map}
   end
 
-  defp generate_next_ins_records(metrics, tag_maps, tsg_ref) do
+  defp generate_next_records(metrics, tag_maps, tsg_ref) do
     timestamp = TimestampGenerator.next_timestamp(tsg_ref)
 
     Enum.map(metrics, fn metric -> %Beiin.Record{metric: metric, timestamp: timestamp} end)
@@ -40,13 +47,32 @@ defmodule RecordServer do
   def handle_call({:next_insert}, _from, map) do
     {:ok, metrics} = Map.fetch(map, :metrics)
     {:ok, tag_maps} = Map.fetch(map, :tag_maps)
-    {:ok, tsg_ref} = Map.fetch(map, :tsg_ref)
+    {:ok, ins_tsg} = Map.fetch(map, :ins_tsg)
 
     {next, new_map} =
       Map.get_and_update(map, :next_ins_records, fn records ->
         [next | rs] =
           case records do
-            [] -> generate_next_ins_records(metrics, tag_maps, tsg_ref)
+            [] -> generate_next_records(metrics, tag_maps, ins_tsg)
+            _ -> records
+          end
+
+        {next, rs}
+      end)
+
+    {:reply, next, new_map}
+  end
+
+  def handle_call({:next_read}, _from, map) do
+    {:ok, metrics} = Map.fetch(map, :metrics)
+    {:ok, tag_maps} = Map.fetch(map, :tag_maps)
+    {:ok, read_tsg} = Map.fetch(map, :read_tsg)
+
+    {next, new_map} =
+      Map.get_and_update(map, :next_read_records, fn records ->
+        [next | rs] =
+          case records do
+            [] -> generate_next_records(metrics, tag_maps, read_tsg)
             _ -> records
           end
 
--- test/lib/record_server_test.exs
+++ test/lib/record_server_test.exs
@@ -10,14 +10,14 @@ defmodule RecordServerTest do
        ) do
     rserver_spec = %{
       id: RecordServer,
-      start: {RecordServer, :start_link, [metrics, tags, [ins_tsg: TimestampGenerator]]}
+      start: {RecordServer, :start_link, [metrics, tags]}
     }
 
     tsg_spec = %{
       id: TimestampGenerator,
       start:
         {TimestampGenerator, :new,
-         [start_time, interval, record_count, [name: TimestampGenerator]]}
+         [start_time, interval, record_count, name: TimestampGenerator]]}
     }
 
     start_supervised!(tsg_spec)
@@ -100,6 +100,91 @@ defmodule RecordServerTest do
       m = length(tags)
 
       records = Enum.map(0..(n * m), fn _ -> RecordServer.next_insert(rserver) end)
+
+      record_metrics = Enum.map(records, fn record -> record.metric end)
+      record_tags = Enum.map(records, fn record -> record.tags end)
+
+      assert Enum.all?(metrics, fn m -> Enum.member?(record_metrics, m) end)
+      assert Enum.all?(tags, fn t -> Enum.member?(record_tags, t) end)
+    end
+  end
+
+  describe "Calling RecordServer.next_read" do
+    test "with 1 metric returns record for that metric" do
+      metrics = ["test_metric_1"]
+      rserver = start_supervised_record_server(metrics)
+
+      record = RecordServer.next_read(rserver)
+      assert record.metric == hd(metrics)
+    end
+
+    test "with 1 tag map returns record with that tag map" do
+      metrics = ["test_metric_2"]
+      tags = [%{test: "test_tag"}]
+      rserver = start_supervised_record_server(metrics, tags)
+
+      record = RecordServer.next_read(rserver)
+      assert record.tags == hd(tags)
+    end
+
+    test "with N metrics and 1 tag map N times returns record for each metric" do
+      metrics = ["test_metric_1", "test_metric_2"]
+      rserver = start_supervised_record_server(metrics)
+
+      records = [RecordServer.next_read(rserver), RecordServer.next_read(rserver)]
+
+      record_metrics = Enum.map(records, fn r -> r.metric end)
+      assert Enum.all?(metrics, fn metric -> Enum.member?(record_metrics, metric) end)
+    end
+
+    test "with N metrics and 1 tag map N*2 times returns 2 records for each metric" do
+      metrics = ["test_metric_1", "test_metric_2"]
+      rserver = start_supervised_record_server(metrics)
+
+      records =
+        Enum.map(metrics, fn _ -> [RecordServer.next_read(rserver), RecordServer.next_read(rserver)] end)
+        |> List.flatten()
+
+      record_metrics = Enum.map(records, fn r -> r.metric end)
+
+      assert Enum.all?(metrics, fn metric ->
+               Enum.count(record_metrics, fn record_metric -> metric == record_metric end)
+             end)
+    end
+
+    test "with N metrics and 1 tag map N times returns N records with start_time" do
+      metrics = ["test_metric_1", "test_metric_2"]
+      tags = [%{}]
+      start_time = 1234
+      rserver = start_supervised_record_server(metrics, tags, start_time)
+
+      records = [RecordServer.next_read(rserver), RecordServer.next_read(rserver)]
+      assert Enum.all?(records, fn record -> record.timestamp == start_time end)
+    end
+
+    test "with N metrics and 1 tag map N*2 times returns sets of timestamps incremented by interval" do
+      metrics = ["test_metric_1", "test_metric_2"]
+      tags = [%{}]
+      start_time = 1234
+      interval = 4444
+      rserver = start_supervised_record_server(metrics, tags, start_time, interval)
+
+      records1 = [RecordServer.next_read(rserver), RecordServer.next_read(rserver)]
+      records2 = [RecordServer.next_read(rserver), RecordServer.next_read(rserver)]
+
+      assert Enum.all?(records1, fn record -> record.timestamp == start_time end)
+      assert Enum.all?(records2, fn record -> record.timestamp == start_time + interval end)
+    end
+
+    test "with N metrics and M tag maps N*M times returns records for cartesian product of metrics and tags" do
+      metrics = ["test_metric_1", "test_metric_2", "test_metric_3"]
+      tags = [%{host: "test_host_1"}, %{host: "test_host_2"}]
+      rserver = start_supervised_record_server(metrics, tags)
+
+      n = length(metrics)
+      m = length(tags)
+
+      records = Enum.map(0..(n * m), fn _ -> RecordServer.next_read(rserver) end)
 
       record_metrics = Enum.map(records, fn record -> record.metric end)
       record_tags = Enum.map(records, fn record -> record.tags end)
